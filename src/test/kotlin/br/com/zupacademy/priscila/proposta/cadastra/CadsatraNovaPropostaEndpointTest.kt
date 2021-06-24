693189d24792a544ff014d768d8f174ac699e674
Priscila Ferreira <priscilasanfer@gmail.com>
2021-06-23 22:07:03 -0300

    Adiciona testes sobre validação de proposta unica por documento

--- src/test/kotlin/br/com/zupacademy/priscila/proposta/cadastra/CadsatraNovaPropostaEndpointTest.kt
+++ src/test/kotlin/br/com/zupacademy/priscila/proposta/cadastra/CadsatraNovaPropostaEndpointTest.kt
@@ -2,6 +2,7 @@ package br.com.zupacademy.priscila.proposta.cadastra
 
 import br.com.zupacademy.priscila.NovaPropostaRequest
 import br.com.zupacademy.priscila.PropostaServiceGrpc
+import br.com.zupacademy.priscila.proposta.Proposta
 import br.com.zupacademy.priscila.proposta.PropostaRepository
 import io.grpc.ManagedChannel
 import io.grpc.Status
@@ -18,6 +19,7 @@ import org.junit.jupiter.params.provider.CsvSource
 import java.util.*
 import javax.inject.Singleton
 import org.junit.jupiter.api.assertThrows
+import java.math.BigDecimal
 
 @MicronautTest(transactional = false)
 internal class CadsatraNovaPropostaEndpointTest(
@@ -51,11 +53,12 @@ internal class CadsatraNovaPropostaEndpointTest(
 
     @ParameterizedTest
     @CsvSource(
-        "123.456.789-10, Nasxiga Naumere, nasxiga.naumere@teste.com, Rua Um 17, 2500.00, documento: Documento invalido (123.456.789-10)",
-        "512.642.090-96, '' , nasxiga.naumere@teste.com, Rua Um 17, 2500.00, nome: não deve estar em branco",
-        "512.642.090-96, Nasxiga Naumere , '', Rua Um 17, 2500.00, email: não deve estar em branco",
-        "512.642.090-96, Nasxiga Naumere , nasxiga.naumere@teste.com, '', 2500.00, endereco: não deve estar em branco",
-        "512.642.090-96, Nasxiga Naumere , nasxiga.naumere@teste.com, Rua Um 17, 0, salario: deve ser maior que 0, endereco: não deve estar em branco"
+        "123.456.789-10, Nasxiga Naumere, nasxiga.naumere@teste.com, Rua Um 17, 2500.00, cadastra.novaProposta.documento: Documento invalido (123.456.789-10)",
+        "512.642.090-96, '' , nasxiga.naumere@teste.com, Rua Um 17, 2500.00, cadastra.novaProposta.nome: não deve estar em branco",
+        "512.642.090-96, Nasxiga Naumere , '', Rua Um 17, 2500.00, cadastra.novaProposta.email: não deve estar em branco",
+        "512.642.090-96, Nasxiga Naumere , nasxiga.naumere@teste.com, '', 2500.00, cadastra.novaProposta.endereco: não deve estar em branco",
+        "512.642.090-96, Nasxiga Naumere , nasxiga.naumere@teste.com, Rua Um 17, 0, cadastra.novaProposta.salario: deve ser maior que 0, endereco: não deve estar em branco",
+//        "512.642.090-96, Nasxiga Naumere , nasxiga.naumere@teste.com, Rua Um 17, '', cadastra.novaProposta.salario: deve ser maior que 0, endereco: não deve estar em branco"
     )
     internal fun `nao deve cadastrar proposta quando algum dado de entrada estiver invalido`(
         documento: String,
@@ -82,6 +85,37 @@ internal class CadsatraNovaPropostaEndpointTest(
             assertEquals(Status.INVALID_ARGUMENT.code, status.code)
             assertEquals(mensagem, status.description)
         }
+    }
+
+    @Test
+    internal fun `nao deve cadastrar proposta quando ja houver proposta com o mesmo documento`() {
+        val existente = repository.save(
+            Proposta(
+                documento = "512.642.090-96",
+                email = "nasxiga.naumere@teste.com",
+                nome = "Nasxiga Naumere",
+                salario = BigDecimal("2500.00"),
+                endereco = "Rua Um 17"
+            )
+        )
+
+        val request = NovaPropostaRequest.newBuilder()
+            .setDocumento("512.642.090-96")
+            .setNome("Nasxiga Naumere")
+            .setEmail("nasxiga.naumere@teste.com")
+            .setEndereco("Rua Um 17")
+            .setSalario("2500.00")
+            .build()
+
+        val error = assertThrows<StatusRuntimeException> {
+            grpcClient.cadastra(request)
+        }
+
+        with(error) {
+            assertEquals(Status.ALREADY_EXISTS.code, status.code)
+            assertEquals("Prosta já existe com o documento '${request.documento}' informado", status.description)
+        }
+
     }
 
     @Factory
